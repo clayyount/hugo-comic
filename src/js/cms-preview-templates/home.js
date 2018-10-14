44b7ea5bc96c986720df54d08b029bffd91b19bf
Clay Yount <clayyount@gmail.com>
2018-10-14 15:55:06 -0400

    added comic post type

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -12,7 +12,7 @@ export default class PostPreview extends React.Component {
         image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
     return <div>
-        <div class="jumbotron">
+        <div class="jumbotron" style="background-image: url({image})">
           <h1>
             {entry.getIn(["data", "title"])}
           </h1>
