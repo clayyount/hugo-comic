85c7b2d77926b940e581afdc0436d39dd8e76ca4
Clay Yount <clayyount@gmail.com>
2018-10-14 15:14:04 -0400

    added comic post type

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -4,7 +4,7 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
+    let image = getAsset(entry.getIn(["data", "banner"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
