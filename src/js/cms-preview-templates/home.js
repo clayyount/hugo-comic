13f7d548741c2bb3b5964c17a2731b4929ff3131
Clay Yount <clayyount@gmail.com>
2018-10-14 16:09:16 -0400

    added comic post type

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -4,15 +4,16 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "banner"]));
+    let imageCSS = getAsset(entry.getIn(["data", "banner"]));
     
 
-    // Bit of a nasty hack to make relative paths work as expected as a background image here
-    if (image && !image.fileObj) {
-        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    // Bit of a nasty hack to make relative paths work as expected as a background imageCSS here
+    if (imageCSS && !imageCSS.fileObj) {
+        imageCSS = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
+    imageCSS="background-image:url("+imageCSS+");"
     return <div>
-        <div class="jumbotron" style='background-image:url({image})'>
+        <div class="jumbotron" style={imageCSS})>
           <h1>
             {entry.getIn(["data", "title"])}
           </h1>
