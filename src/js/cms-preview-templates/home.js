4cd924d47c99b94dfa3dda91ce91b47605bb99d9
Clay Yount <clayyount@gmail.com>
2018-10-14 15:43:49 -0400

    added comic post type

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -1,33 +1,23 @@
 import React from "react";
 import format from "date-fns/format";
-var jumboHTML=='<div class="jumbotron"';
+
 export default class PostPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "banner"]));
-    let title = getAsset(entry.getIn(["data", "title"]));
-    let subtitle = getAsset(entry.getIn(["data", "subtitle"]));
+    let image = getAsset(entry.getIn(["data", "banner"])) || "";
+    let title = getAsset(entry.getIn(["data", "title"])) || "";
+    let subtitle = getAsset(entry.getIn(["data", "subtitle"])) || "";
     
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
         image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
-    if(image){
-      jumboHTML+=' style="background-image: url('+image+')"';
-    }
-    jumboHTML+='>';
-    if(title){
-      jumboHTML+='<h1>'+title+'</h1>';
-    }
-    if(subtitle){
-      jumboHTML+='<p>'+subtitle+'</p>';
-    }
-    jumboHTML+='</div>';
-
     return <div>
    
-        {jumboHTML}
+        {image}
+        {title}
+        {subtitle}
         <main>
 
         </main>
