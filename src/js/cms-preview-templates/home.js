7c0f6c2212df8ea0083f758f785aff455ae0c9eb
Clay Yount <clayyount@gmail.com>
2018-10-14 15:35:58 -0400

    added comic post type

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -7,15 +7,27 @@ export default class PostPreview extends React.Component {
     let image = getAsset(entry.getIn(["data", "banner"]));
     let title = getAsset(entry.getIn(["data", "title"]));
     let subtitle = getAsset(entry.getIn(["data", "subtitle"]));
+    let jumboHTML=='<div class="jumbotron"'
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
         image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
+    if(image){
+      jumboHTML+=' style="background-image: url('+image+')"';
+    }
+    jumboHTML+='>';
+    if(title){
+      jumboHTML+='<h1>'+title+'</h1>';
+    }
+    if(subtitle){
+      jumboHTML+='<p>'+subtitle+'</p>';
+    }
+    jumboHTML+='</div>';
 
     return <div>
    
-        {image}
+        {jumboHTML}
         <main>
 
         </main>
