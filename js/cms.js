58842b8ff19edb15472b7b71cdd82b89852796da
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-19 13:03:50 -0400

    *

--- js/cms.js
+++ js/cms.js
@@ -2,7 +2,7 @@ import React from "react";
 import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
+import BlogPreview from "./cms-preview-templates/blog";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
@@ -22,7 +22,7 @@ class ColorControl extends React.Component {
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("blog", PostPreview);
+CMS.registerPreviewTemplate("blog", BlogPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
