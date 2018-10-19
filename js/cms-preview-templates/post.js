19e19a1a9fe8e9addffa36424604ad74d66c9dff
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-19 13:10:52 -0400

    *

--- js/cms-preview-templates/post.js
+++ js/cms-preview-templates/post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import format from "date-fns/format";
 
-export default class BlogPreview extends React.Component {
+export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
