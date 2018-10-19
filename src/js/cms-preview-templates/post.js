841fb6cbb791573b98d75e46b1721ed8a00e45d1
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-19 13:45:20 -0400

    *

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -6,7 +6,8 @@ export default class PostPreview extends React.Component {
     var entry = this.props.entry;
     var image = entry.getIn(['data', 'image']);
     var bg = this.props.getAsset(image) || "";
-    return h('div', {},
+    return h('main', {},
+      h('div', {"className": "content"},
       h('h1', {}, entry.getIn(['data', 'title'])),
       h('img', {src: bg.toString()}),
       h('div', {"className": "text"}, this.props.widgetFor('body'))
