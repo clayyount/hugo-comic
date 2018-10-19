6f74619fa4e2dd9cb37cdd62ffe096b8a59d42f0
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-19 13:37:11 -0400

    *

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -7,16 +7,6 @@ export default class PostPreview extends React.Component {
     let image = getAsset(entry.getIn(["data", "image"]));
 
     return 
-      <main>
-      <div className="content">
-      <h1>{ entry.getIn(["data", "title"])}</h1>
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-      </div>
-      <div>
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
-      </div>
-      </main>;
+      { widgetFor("body") };
   }
 }
