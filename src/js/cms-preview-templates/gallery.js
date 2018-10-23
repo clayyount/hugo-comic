b8b43b9dd06456df21b22299a99662933f05d1df
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-23 11:41:44 -0400

    *

---
+++ src/js/cms-preview-templates/gallery.js
@@ -0,0 +1,29 @@
+import React from "react";
+import format from "date-fns/format";
+
+export default class PostPreview extends React.Component {
+  render() {
+    var entry = this.props.entry;
+    var galleryHTML = '';
+    var galleries = entry.getIn(['data', 'galleries']);
+    for(var i=0;i<galleries.length;i++){
+      var rowNum=galleries[i].getIn(['data', 'rownum']);
+      var bgSize='contain';
+      if(galleries[i].getIn(['data', 'fullimage'])){
+        bgSize='cover';
+      }
+      galleryHTML+='<section class="flex gallery">';
+      galleryHTML+='<h1>'+galleries.getIn(['data', 'title'])+'</h1>';
+      var entries = galleries[i].getIn(['data', 'gallery']);
+      for(var j=0;j<entries.length;j++){
+        galleryHTML+='<article class="image-holder cols-'+rowNum+'">';
+        galleryHTML+='<div style="background-image:url('+entries[j].getIn(['data', 'image'])+');background-size:'+bgSize+'" data-image="'+entries[j].getIn(['data', 'image'])+'" class="image"></div>';
+        galleryHTML+='</article>';
+      }
+      galleryHTML+='</section>';
+    }
+    return h('main', {},
+      h('div', {"className": "content"},
+      h('div', {"className": "galleries"}, galleryHTML)));
+  }
+}
--- src/js/cms.js
+++ src/js/cms.js
@@ -3,6 +3,8 @@ import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
+import GalleryPreview from "./cms-preview-templates/gallery";
+
 
 
 // Example of creating a custom color widget
@@ -21,6 +23,7 @@ CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewStyle("https://fonts.googleapis.com/css?family=Roboto|Teko");
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("blog", PostPreview);
+CMS.registerPreviewTemplate("gallery", GalleryPreview);
 CMS.registerPreviewTemplate("hamlets-danish-comic", PostPreview);
 CMS.registerPreviewTemplate("rae-comic", PostPreview);
 CMS.registerWidget("color", ColorControl);
