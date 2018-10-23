87de828f950b4f507847154664b5144f076fe2ba
Yount, Clay <Clay.Yount@ellucian.com>
2018-10-23 12:04:11 -0400

    *

--- src/js/cms-preview-templates/gallery.js
+++ src/js/cms-preview-templates/gallery.js
@@ -4,7 +4,7 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     var entry = this.props.entry;
-    var galleryHTML = '';
+    var galleryHTML = [<main><div class="content">];
     var galleries = entry.getIn(['data', 'galleries']);
     for(var i=0;i<galleries.length;i++){
       var rowNum=galleries[i].getIn(['data', 'rownum']);
@@ -12,19 +12,16 @@ export default class PostPreview extends React.Component {
       if(galleries[i].getIn(['data', 'fullimage'])){
         bgSize='cover';
       }
-      galleryHTML+='<section class="flex gallery">';
-      galleryHTML+='<h1>'+galleries.getIn(['data', 'title'])+'</h1>';
+      galleryHTML.push(<section class="flex gallery">);
+      galleryHTML.push(<h1>{galleries.getIn(['data', 'title'])}</h1>);
       var entries = galleries[i].getIn(['data', 'gallery']);
       for(var j=0;j<entries.length;j++){
-        galleryHTML+='<article class="image-holder cols-'+rowNum+'">';
-        galleryHTML+='<div style="background-image:url('+entries[j].getIn(['data', 'image'])+');background-size:'+bgSize+'" data-image="'+entries[j].getIn(['data', 'image'])+'" class="image"></div>';
-        galleryHTML+='</article>';
+        galleryHTML.push(<article class="image-holder cols-{rowNum}">);
+        galleryHTML.push(<div style="background-image:url({ entries[j].getIn(['data', 'image'])});background-size:{bgSize}" data-image="{entries[j].getIn(['data', 'image'])}" class="image"></div>';
+        galleryHTML.push(</article>);
       }
-      galleryHTML+='</section>';
-      console.log(galleryHTML);
+      galleryHTML.push(</section></div></main>);
     }
-    return h('main', {},
-      h('div', {"className": "content"},
-      h('div', {"className": "galleries"}, galleryHTML)));
+    return galleryHTML
   }
 }
